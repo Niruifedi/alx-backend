7f30ea59de692ec1c8a1cd34a656ea9f325befc4
Niruifedi <ifediniruizuchukwu@gmail.com>
2023-05-02 11:02:14 +0100

    trnslate

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Basic flask app
+"""
+from flask_babel import (
+    Babel,
+    _)
+from flask import (
+    Flask,
+    render_template,
+    request)
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    class config to set the languages and default time zone
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'fr'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    fuction to determine the best match with our supported languages
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def index() -> str:
+    """
+    function returns a basic html file
+    """
+    return  render_template(_("3-index.html"))
